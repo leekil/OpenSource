2c9f33ec7901279d804eccf18718e14a348c24d1
leekil <dlrlf369@naver.com>
2016-09-20 18:25:01 +0900

    Add files via upload

---
+++ char.c
@@ -0,0 +1,16 @@
+#include	<stdio.h>
+
+int main(void)
+
+{
+	char c1 = 'a';          
+	char c2 = 65;
+	char c3 = '\132';
+	char c4 = '\x5A';
+
+	printf("저장값(문자): %C %C %C %C\n", c1, c2, c3, c4);
+	printf("저장값(정수): %d %d %d %d\n", c1, c2, c3, c4);
+
+
+	return 0;
+}
